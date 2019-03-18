931a394552e5a9820e67c87c6113b7cb380f6e22
abhijeet <abhijeet1069@gmail.com>
2019-03-18 09:27:49 +0530

    17th march 19

---
+++ product.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int n, a[1000];
+  long int product = 1;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++)
+    scanf("%d", a+i);
+  for(int i = 0; i < n; i++)
+    product = (product*a[i]) % (1000000000 + 7);
+  printf("%ld\n", product);
+  return 0;
+}
